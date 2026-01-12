a3cad940a3f5ed226498899d379fdb566beddd76
jklins-ects <73347118+jklins-ects@users.noreply.github.com>
2026-01-12 08:36:45 -0500

    added more prints

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 print("Hello I'm here for git practice - Local")
+print("Sometimes I think about the dream of being a walmart greeter")
 print("Hello I'm here for git practice - Remote")
+print("Sometimes I realize I want to just play with electronics all day.")
 print("Added during merge")
+print("Local end")
